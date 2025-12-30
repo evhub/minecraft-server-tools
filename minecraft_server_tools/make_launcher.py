a8e265373249f7e1445ee1a6c3051147d7fe66c2
Evan Hubinger <evanjhub@gmail.com>
2025-12-29 18:35:49 -0800

    Recompile

--- minecraft_server_tools/make_launcher.py
+++ minecraft_server_tools/make_launcher.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# __coconut_hash__ = 0xf2cb9b39
+# __coconut_hash__ = 0x82165f02
 
-# Compiled with Coconut version 3.1.1-post_dev3
+# Compiled with Coconut version 3.1.2-post_dev9
 
 # Coconut Header: -------------------------------------------------------------
 
 from __future__ import print_function, absolute_import, unicode_literals, division
 import sys as _coconut_sys
 import os as _coconut_os
-_coconut_header_info = ('3.1.1-post_dev3', '', True)
+_coconut_header_info = ('3.1.2-post_dev9', '', True)
 _coconut_cached__coconut__ = _coconut_sys.modules.get(str('__coconut__'))
 _coconut_file_dir = _coconut_os.path.dirname(_coconut_os.path.abspath(__file__))
 _coconut_pop_path = False
